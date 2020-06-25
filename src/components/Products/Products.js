e95a6356905db9f145692b76556b9885896252b5
Mohammed Shousha <mshousha29@gmail.com>
2020-06-25 16:20:58 +0200

    Editing Products

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,48 +7,61 @@ import p2 from '../Slideshow/3.jpg'
 import p3 from '../Slideshow/7.jpg'
 import p4 from '../Slideshow/2.jpg'
 
-const photos1 = [p1, p2]
-const photos2 = [p3, p4]
+const P1 = [{
+	photos : [p1, p2],
+	price : '100 EGP',
+	productName : 'Headset',
+},
+{
+	photos:[p3, p4],
+	price: '20000 EGP',
+	productName: 'IPhone11'
+}]
+
+const P2 = [{
+	photos : [p2, p1],
+	price : '100 EGP',
+	productName : 'Headset',
+},
+{
+	photos:[p4, p3],
+	price: '20000 EGP',
+	productName: 'IPhone11'
+}]
+
+const PRO1 = [P1, P2] , PRO2 = [P2, P1]
+
+
+const PRODUCTS = [PRO1, PRO2]
 
 
 const Products =()=>{
 	return(
-		<div className='container'>
-			<div className='cards'>
-				<ProductCard
-				 photos={photos1}
-				 price='100$'
-				 productName='Beats Headset'
-				 buttonText='Add to Cart'
-				 url='cart'
-				 float={true}/>
-				 <ProductCard
-				 photos={photos2}
-				 price='1000$'
-				 productName='IPhone11'
-				 buttonText='Add to Cart'
-				 url='cart'
-				 float={true}/>
-			</div>
+		<div>
+			{PRODUCTS.map(PRO=>{
+				return(
+					<div className='row'>
+						{PRO.map(P =>{
+							return(
+								<div className='h-row'>
+									{P.map(product =>{
+										return(
+											<ProductCard 
+											photos = {product.photos}
+											price = {product.price}
+											productName = {product.productName}
+											buttonText = 'Add to Cart'
+											float={true}/>
+										)
+									})}
+								</div>
+							)
+						})}
+					</div>	
+				)
 
-			<div className='cards'>
-				 <ProductCard
-				 photos={photos1}
-				 price='100$'
-				 productName='Beats Headset'
-				 buttonText='Add to Cart'
-				 url='cart'
-				 float={true}/>
-				 <ProductCard
-				 photos={photos2}
-				 price='1000$'
-				 productName='IPhone11'
-				 buttonText='Add to Cart'
-				 url='cart'
-				 float={true}/>
-			</div>
+			})}
 		</div>
-
 
 	);
 }

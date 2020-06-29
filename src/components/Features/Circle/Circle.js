c21b54f63197c74fcf4a767a43a45a40e11a4ffc
Mohammed Shousha <mshousha29@gmail.com>
2020-06-29 15:31:43 +0200

    Editing Features

--- src/components/Features/Circle/Circle.js
+++ src/components/Features/Circle/Circle.js
@@ -7,7 +7,7 @@ const Circle = ({tag, img, color, description}) =>{
 		<div className='circle' style={{background:color}}>
 			<div className='pa1 tooltip'>
 				<h3 className='tag'> {tag} 
-				<p className='tooltiptext' style={{color:color}}> {description} </p></h3>
+				<p className='tooltiptext up' style={{color:color}}> {description} </p></h3>
 				<img className='icons' src ={img} alt ='feature'/>
 			</div>
 		</div>
